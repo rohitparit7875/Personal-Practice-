fc379bbf6d453b9774b70fcd74f662dfe79caa37
Rohit Parit <rohitparit70@gmail.com>
2025-09-09 23:21:16 +0530

    Prime.py
    Signed-off-by: Rohit Parit <rohitparit70@gmail.com>

---
+++ Prime.py
@@ -0,0 +1,11 @@
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+# Example
+print(is_prime(7))   # True
+print(is_prime(10))  # False
